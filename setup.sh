752b0865aa9d22b9f8ec00d9783e4b9930262d80
H2CK <H2CK@users.noreply.github.com>
2018-09-04 13:35:07 +0200

    Updated to new server location

--- setup.sh
+++ setup.sh
@@ -31,26 +31,26 @@ chmod -R +x /etc/service/ /etc/my_init.d/
 apt-get update -qq
 apt-get install -qy wget unzip  xz-utils
 
-TWONKY_URL=$(curl -sL http://twonky.com/downloads/ | sed -nr 's#.*href="(.+?/twonky-i686-glibc-.+?\.zip)".*#\1#p')
-TWONKY_VERSION=$(echo $TWONKY_URL | sed -nr 's#.*twonky-i686-glibc-.+?-(.+?)\.zip.*#\1#p')
-TWONKY_URL=$(curl -sL http://twonkyforum.com/downloads/$TWONKY_VERSION/ | sed -nr 's#.*href="(twonky-x86-64-glibc-.+?\.zip)".*#http://twonkyforum.com/downloads/'$TWONKY_VERSION'/\1#p')
+TWONKY_URL=$(curl -sL http://twonky.com/downloads/ | sed -nr 's#.*href="(.+?/twonky-x86-64-glibc-.+?\.zip)".*#\1#p')
+TWONKY_VERSION=$(echo $TWONKY_URL | sed -nr 's#.*twonky-x86-64-glibc-.+?-(.+?)\.zip.*#\1#p')
+TWONKY_URL=$(curl -sL http://download.twonky.com/$TWONKY_VERSION/ | sed -nr 's#.*href="(twonky-x86-64-glibc-.+?\.zip)".*#http://download.twonky.com/'$TWONKY_VERSION'/\1#p')
 TWONKY_ZIP=/tmp/twonkyserver_$TWONKY_VERSION.zip
 TWONKY_DIR=/usr/local/twonky
 
 wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -O /tmp/ffmpeg.tar.xz
 wget -q "$TWONKY_URL" -O $TWONKY_ZIP
-if [ $? -eq 0 ]; then
-    mkdir -p $TWONKY_DIR
-    unzip -d $TWONKY_DIR -o $TWONKY_ZIP
-    rm -f $TWONKY_ZIP
-    chmod -R +x $TWONKY_DIR
-    chown -R root:users /config
-    chmod 770 /config
-    tar -C /tmp/ -xvf /tmp/ffmpeg.tar.xz
-    cd /tmp/ffmpeg* 
-    rm -R manpages
-    cp * $TWONKY_DIR/cgi-bin/
-fi
+
+mkdir -p $TWONKY_DIR
+unzip -d $TWONKY_DIR -o $TWONKY_ZIP
+rm -f $TWONKY_ZIP
+chmod -R +x $TWONKY_DIR
+chown -R root:users /config
+chmod 770 /config
+tar -C /tmp/ -xvf /tmp/ffmpeg.tar.xz
+cd /tmp/ffmpeg* 
+rm -R manpages
+cp * $TWONKY_DIR/cgi-bin/
+
 
 # Clean APT install files
 apt-get clean -y
